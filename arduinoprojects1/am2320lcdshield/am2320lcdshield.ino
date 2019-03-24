8fbf891b41a01a0236e2a7806ab78d6eb72547ce
getelectronics <30749112+getelectronics@users.noreply.github.com>
2019-03-24 15:51:32 +0000

    Add files via upload

---
+++ arduinoprojects1/am2320lcdshield/am2320lcdshield.ino
@@ -0,0 +1,78 @@
+//You will need to install the Adafruit GXX and PCD8544 libraries and https://github.com/EngDial/AM2320 for the sensor
+
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+#include <Wire.h>
+#include <AM2320.h>
+
+
+// Software SPI (slower updates, more flexible pin options):
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+Adafruit_PCD8544 display = Adafruit_PCD8544(2, 3, 4, 5, 6); //dfrobot lcd4884 shield
+
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+AM2320 th;
+
+void setup()   
+{
+  Serial.begin(9600);
+  Wire.begin();             // join i2c bus (address optional for master) 
+  display.begin();
+  // init done
+
+  // you can change the contrast around to adapt the display
+  // for the best viewing!
+  display.setContrast(50);
+  display.clearDisplay();   // clears the screen and buffer
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(0,0);
+  display.setTextColor(WHITE, BLACK); // 'inverted' text
+  display.println("AM2302 TEST");
+  display.display();
+  delay(1000);
+
+}
+
+ 
+void loop() 
+{
+
+    // Display temperature
+    display.setTextColor(BLACK, WHITE); // 'inverted' text
+    Serial.println("Chip = AM2320");
+    display.setCursor(0,0);
+    switch(th.Read()) 
+    {
+      case 2:
+        display.println(" CRC failed");
+        break;
+      case 1:
+        display.println(" Sensor offline");
+        break;
+      case 0:
+        display.setCursor(0,15);
+        display.print("Humi = ");
+        display.print(th.Humidity);
+        display.println("%");
+        display.setCursor(0,30);
+        display.print("Temp = ");
+        display.print(th.cTemp);
+        display.println("C");
+        display.println();
+        break;
+    }
+    display.display();
+    delay(1000);
+}
+
+
---
+++ arduinoprojects1/sht31lcd/sht31lcd.ino
@@ -0,0 +1,63 @@
+//You need to add the adafruit sht31 library, this is available in the library manager
+
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#include "Adafruit_SHT31.h"
+
+//setup for the LCD keypad shield
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+void setup() 
+{
+  //init serial for some debug
+  Serial.begin(9600);
+  Serial.println("Light Sensor Test"); 
+  //init sht31
+  if (! sht31.begin(0x44)) 
+  {
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+  //init LCD
+  lcd.begin(16,2);
+  //line 1 - temperature
+  lcd.setCursor(0,0);
+  lcd.print("Temp = ");
+  //line 2 - humidity
+  lcd.setCursor(0,1);
+  lcd.print("Hum = ");
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+  //display temperature on lcd
+  if (! isnan(t)) 
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Temp = "); 
+    lcd.print(t);
+  } 
+  else 
+  { 
+    Serial.println("Failed to read temperature");
+  }
+  //display humidity on lcd  
+  if (! isnan(h)) 
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Hum = ");
+    lcd.print(h);
+  } 
+  else 
+  { 
+    Serial.println("Failed to read humidity");
+  }
+  Serial.println();
+  delay(500);
+}
+
+
