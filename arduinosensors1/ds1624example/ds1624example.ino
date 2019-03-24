24c9eab374adf46e873459df769ad17f60c2da1e
getelectronics <30749112+getelectronics@users.noreply.github.com>
2019-03-24 15:51:59 +0000

    Add files via upload

---
+++ arduinosensors1/ds1624example/ds1624example.ino
@@ -0,0 +1,29 @@
+//I downloaded and imported the following library - https://github.com/bluemurder/DS1624 This is the example so some of the hard graft has already been done for you 
+
+#include <DS1624.h>
+
+// Sensor presents all address pins connected to ground
+DS1624 ds1624(0x00);
+
+void setup()
+{ 
+// Begin serial connection at 9600 baud
+Serial.begin(9600);
+}
+
+void loop()
+{
+float temperature;
+bool valid;
+
+// Get current temperature
+ds1624.GetTemperature(temperature, valid);
+
+// Print it
+Serial.println(temperature);
+
+// Wait a while
+delay(1000);
+}
+
+
---
+++ arduinosensors1/hmc5983example/hmc5983example.ino
@@ -0,0 +1,42 @@
+#include <Wire.h> //I2C Arduino Library
+#define addr 0x1E //I2C Address for The HMC5883
+
+void setup()
+{
+Serial.begin(9600);
+Wire.begin();
+Wire.beginTransmission(addr);
+Wire.write(0x02);
+Wire.write(0x00); //Continuously Measure
+Wire.endTransmission();
+}
+
+void loop()
+{
+int x,y,z; //triple axis data
+Wire.beginTransmission(addr);
+Wire.write(0x03);
+Wire.endTransmission();
+//Read the data
+Wire.requestFrom(addr, 6);
+if(6<=Wire.available())
+{
+  x = Wire.read()<<8; //MSB  x 
+  x |= Wire.read(); //LSB  x
+  z = Wire.read()<<8; //MSB  z
+  z |= Wire.read(); //LSB z
+  y = Wire.read()<<8; //MSB y
+  y |= Wire.read(); //LSB y
+}
+// Show Values
+Serial.print("X Value: ");
+Serial.println(x);
+Serial.print("Y Value: ");
+Serial.println(y);
+Serial.print("Z Value: ");
+Serial.println(z);
+Serial.println();
+delay(500);
+}
+
+
